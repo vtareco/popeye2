0c20c1787c04d99e6f1f4ffdb9156790c61a9671
xupmabuzz <xupmabuzz@gmail.com>
2019-02-01 15:36:20 +0000

    adaptado a everis centers. jenix settings v1 (sem json parse a folha)

---
+++ src/main/java/net/dms/fsync/synchronizer/LocalVariables/business/VariableService.java
@@ -0,0 +1,75 @@
+package net.dms.fsync.synchronizer.LocalVariables.business;
+
+
+import net.dms.fsync.httphandlers.common.Utils;
+import net.dms.fsync.settings.entities.EverisConfig;
+import net.dms.fsync.settings.entities.EverisPropertiesType;
+import net.dms.fsync.settings.entities.EverisVariables;
+import net.dms.fsync.synchronizer.LocalVariables.control.LocalVariables;
+import net.dms.fsync.synchronizer.LocalVariables.entities.ApplicationProperties;
+import net.dms.fsync.synchronizer.LocalVariables.entities.UserChange;
+
+
+
+public class VariableService {
+
+    LocalVariables localVariables = new LocalVariables();
+
+    public UserChange getUserVariables(){
+        UserChange uc = new UserChange();
+        uc.setFenixUser(localVariables.getVariables().get(EverisVariables.FENIX_USER.getVariableName()));
+        uc.setFenixPassword(localVariables.getVariables().get(EverisVariables.FENIX_PASSWORD.getVariableName()));
+
+        uc.setJirauser(localVariables.getVariables().get(EverisVariables.JIRA_USER.getVariableName()));
+        uc.setJiraPassword(localVariables.getVariables().get(EverisVariables.JIRA_PASSWORD.getVariableName()));
+
+        return uc;
+    }
+
+
+
+
+    /*
+    public void writeUserConfJsonFile(UserChange uc){
+
+    }
+
+    public void getUserJsonsVariables(){
+
+    }
+
+
+
+
+    public  void readJsonToConfFile(File jsonUserCreate, File jsonApplicationProperties){
+        try {
+            Object obj = new JSONParser().parse(new FileReader(jsonUserCreate));
+            JSONObject jsonUserChange = (JSONObject) obj;
+
+            ApplicationProperties ap = new ApplicationProperties();
+            String file = EverisConfig.getInstance().getProperty(EverisPropertiesType.FENIX_USER);
+            file = Utils.replaceVariable("fenixUser",file,jsonUserChange.get("fenixUser").toString());
+
+            System.out.println(jsonUserChange.get("fenixUser").toString());
+            System.out.println(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+*/
+
+    public ApplicationProperties getApplicationVariables(){
+        ApplicationProperties ap = new ApplicationProperties();
+        ap.setFenirUrl(localVariables.getVariables().get(EverisVariables.FENIX_URL_BASE.getVariableName()));
+        ap.setJiraUrl(localVariables.getVariables().get(EverisVariables.JIRA_URL_BASE.getVariableName()));
+        ap.setProxyHost(localVariables.getVariables().get(EverisVariables.PROXY_HOST.getVariableName()));
+        ap.setProxyPort(localVariables.getVariables().get(EverisVariables.PROXY_PORT.getVariableName()));
+        ap.setWorkingDirectory(localVariables.getVariables().get(EverisVariables.WORKING_DIRECTORY.getVariableName()));
+        return ap;
+    }
+
+
+}

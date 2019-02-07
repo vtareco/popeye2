8c7d4d0749db9074ff8a1dd2e6a2db2e3eb80806
xupmabuzz <xupmabuzz@gmail.com>
2019-02-07 09:54:23 +0000

    adaptado a everis centers. jenix settings v1 (sem json parse a folha)

--- src/main/java/net/dms/fsync/swing/dialogs/SettingsDialog.java
+++ src/main/java/net/dms/fsync/swing/dialogs/SettingsDialog.java
@@ -1,9 +1,11 @@
 package net.dms.fsync.swing.dialogs;
 
+import net.dms.fsync.swing.Panes.FilterManagePane;
 import net.dms.fsync.swing.Panes.ServerChangePane;
 import net.dms.fsync.swing.Panes.UserChangePane;
 import net.dms.fsync.synchronizer.LocalVariables.entities.ApplicationProperties;
 import net.dms.fsync.synchronizer.LocalVariables.entities.JenixSettings;
+import net.dms.fsync.synchronizer.LocalVariables.entities.JsonPaths;
 import net.dms.fsync.synchronizer.LocalVariables.entities.UserChange;
 
 import javax.swing.*;
@@ -12,7 +14,7 @@ import java.awt.*;
 public class SettingsDialog  extends JDialog {
 
     JButton save;
-
+/*
     public SettingsDialog(JTabbedPane tabbedPane, JenixSettings js){
         setLayout(null);
         this.setSize(600, 400);
@@ -23,9 +25,19 @@ public class SettingsDialog  extends JDialog {
 
         loadDialog(js);
     }
+*/
+    public SettingsDialog(JTabbedPane tabbedPane, JsonPaths jsonPaths){
+        setLayout(null);
+        this.setSize(600, 400);
+        setTitle("Jenix Settings");
+        setLocationRelativeTo(tabbedPane);
+        setModal(true);
+        setResizable(false);
 
+        loadDialog(jsonPaths);
+    }
 
-    private void loadDialog(JenixSettings js){
+    private void loadDialog(JsonPaths jsonPaths){
 
         JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
         tabbedPane.setBackground(Color.WHITE);
@@ -33,12 +45,14 @@ public class SettingsDialog  extends JDialog {
         getContentPane().add(tabbedPane);
 
 
-        JPanel userChange = new UserChangePane(js);
+        JPanel userChange = new UserChangePane(jsonPaths.getUserJsonPath());
         tabbedPane.addTab("User Configuration", null, userChange, null);
 
-        JPanel serverChange = new ServerChangePane(js);
+        JPanel serverChange = new ServerChangePane(jsonPaths.getApplicationPropertiesPath());
         tabbedPane.addTab("Application Properties", null, serverChange, null);
 
+        JPanel filterManager = new FilterManagePane(jsonPaths.getFiltersPath());
+        tabbedPane.addTab("Filters",null,filterManager,null);
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
 
---
+++ src/main/java/net/dms/fsync/synchronizer/LocalVariables/entities/JsonPaths.java
@@ -0,0 +1,38 @@
+package net.dms.fsync.synchronizer.LocalVariables.entities;
+
+public class JsonPaths {
+    private String userJsonPath;
+    private String applicationPropertiesPath;
+    private String filtersPath;
+
+    public JsonPaths(String userJsonPath, String applicationPropertiesPath, String filtersPath) {
+        this.userJsonPath = userJsonPath;
+        this.applicationPropertiesPath = applicationPropertiesPath;
+        this.filtersPath = filtersPath;
+    }
+
+
+    public String getUserJsonPath() {
+        return userJsonPath;
+    }
+
+    public void setUserJsonPath(String userJsonPath) {
+        this.userJsonPath = userJsonPath;
+    }
+
+    public String getApplicationPropertiesPath() {
+        return applicationPropertiesPath;
+    }
+
+    public void setApplicationPropertiesPath(String applicationPropertiesPath) {
+        this.applicationPropertiesPath = applicationPropertiesPath;
+    }
+
+    public String getFiltersPath() {
+        return filtersPath;
+    }
+
+    public void setFiltersPath(String filtersPath) {
+        this.filtersPath = filtersPath;
+    }
+}

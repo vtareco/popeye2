20d1c7aaab9698dcd9d92a07a7378f5458c8a0fe
Miro <XMORAB@de.softlab.net>
2019-02-24 23:29:21 +0000

    Added swing version of toast

--- src/main/java/net/dms/fsync/swing/components/MyColors.java
+++ src/main/java/net/dms/fsync/swing/components/MyColors.java
@@ -8,9 +8,11 @@ import java.awt.*;
 public class MyColors {
     public final static Color ACC_NO_EDITABLE = new Color(189,189, 189);
     public final static Color JIRA_CREATED = ACC_NO_EDITABLE;
-    public final static Color TABLE_ROW_ODD = new Color(206,227, 246);
-    public final static Color TABLE_ROW_EVEN = new Color(247,250, 252);
+    final static Color TABLE_ROW_ODD = new Color(206,227, 246);
+    final static Color TABLE_ROW_EVEN = new Color(247,250, 252);
     public final static Color TABLE_FIELD_REQUIRED = new Color(255,0,0);
+    //TOAST
+    final static Color TOAST_ERROR_LINE = new Color(155,0,0);
 
     public final static Color  ROW_SELECTED = new Color(0,0, 255);
 }
---
+++ src/main/java/net/dms/fsync/swing/components/Toast.java
@@ -0,0 +1,100 @@
+package net.dms.fsync.swing.components;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+/**
+ * Toast-like behaviour for swing
+ */
+public class Toast extends JDialog {
+
+	private int miliseconds;
+
+	public Toast(String toastString, int time, ToastType toastType) {
+		this.miliseconds = time;
+		setUndecorated(true);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		JPanel panel = setupContainer(toastType);
+		JLabel toastLabel = setupMessageLabel(toastString);
+		setBounds(panel, toastLabel);
+		destroyWhenTimeIsOver();
+	}
+
+	/**
+	 * Setup and create a container for the children
+	 *
+	 * @return A new {@link JPanel} instance
+	 */
+	private JPanel setupContainer(ToastType toastType) {
+		JPanel panel = new JPanel();
+		panel.setBackground(toastType.bkgColor);
+		panel.setBorder(new LineBorder(toastType.lineColor, 2));
+		getContentPane().add(panel, BorderLayout.CENTER);
+		return panel;
+	}
+
+	/**
+	 * Setup the message provided to appear in the content area
+	 *
+	 * @param toastString The message to display
+	 *
+	 * @return A new {@link JLabel} instance
+	 */
+	private JLabel setupMessageLabel(String toastString) {
+		JLabel toastLabel = new JLabel("");
+		toastLabel.setText(toastString);
+		toastLabel.setFont(new Font("Dialog", Font.BOLD, 12));
+		toastLabel.setForeground(Color.WHITE);
+		return toastLabel;
+	}
+
+	/**
+	 * Change the bounds of this toast
+	 *
+	 * @param panel      The panel that will change size
+	 * @param toastLabel The label to show
+	 */
+	private void setBounds(JPanel panel, JLabel toastLabel) {
+		setSize(toastLabel.getPreferredSize().width + 20, 31);
+		setAlwaysOnTop(true);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation(dim.width / 2 - getSize().width / 2, 0);
+		panel.add(toastLabel);
+		setVisible(true);
+	}
+
+	/**
+	 * Kill this toast when the timer is over
+	 */
+	private void destroyWhenTimeIsOver() {
+		new Thread(() -> {
+			try {
+				Thread.sleep(miliseconds);
+				dispose();
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
+	
+	private enum ToastType {
+		ERROR(Color.RED, MyColors.TOAST_ERROR_LINE),
+		INFO(Color.CYAN, Color.BLUE),
+		WARNING(Color.YELLOW, Color.ORANGE);
+
+		private final Color bkgColor;
+		private final Color lineColor;
+
+		ToastType(Color bkgColor, Color lineColor) {
+			this.bkgColor = bkgColor;
+			this.lineColor = lineColor;
+		}
+	}
+}
